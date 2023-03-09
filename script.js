e881157cf140ff4f97329fd9a88553eb143d2a89
bekzatkazatov <bekzatx2x@gmail.com>
2023-03-10 00:28:12 +0600

    funk2

---
+++ script.js
@@ -0,0 +1,69 @@
+// function handleSum (user){
+//     if (user === 'admin') {
+//         return 'welcome'
+//     } else {
+//         return ` ${user} not correct user`
+//     }
+// }
+//     console.log(handleSum('beka'))
+
+// function  vesna (day){
+//     if (day === 'february'){
+//         return 'on february only 28 day'
+//     }
+//  else {
+//      return `your day is 'day' of another month`
+//  }
+// }
+// console.log(vesna ('february'))
+
+// function number (num){
+//     if (num === '50'){
+//         return 'equals of 50'
+//     } else{
+//         return  ` ${num} is not correct`
+//     }
+// }
+// console.log(number("50"))
+
+
+
+// function  minimum (user) {
+//     if (user.length >= 3)
+//
+//     {
+//         return 'name is accepted'
+//     }
+//     else   {
+//         return 'name is too short'
+//     }
+//
+// }
+// console.log(minimum('fg'))
+
+// function  minimum (user) {
+//     if (user.length > 5)
+//
+//     {
+//         return 'name is to long'
+//     }
+//     else
+//         return 'name is accepted'
+// }
+// console.log(minimum('fg4bv'))
+
+
+
+// function minimum(age) {
+//     if (Number.isInteger(age)) {
+//         if ( age === 20) {
+//             return 'Age is a number'
+//         } else {
+//             return 'Age is not 20'
+//         }
+//     } else {
+//         return 'Age is not a number'
+//     }
+// }
+// console.log(minimum(434))
+
