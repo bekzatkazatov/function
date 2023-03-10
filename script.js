8c340a845002541506d5398490aa6b938d104abf
bekzatkazatov <bekzatx2x@gmail.com>
2023-03-10 21:42:44 +0600

    change

--- script.js
+++ script.js
@@ -12,10 +12,10 @@
 //         return 'today is february'
 //     }
 //  else {
-//      return `your day is 'day' of another month`
+//      return `your day is ${day} of another month`
 //  }
 // }
-// console.log(vesna (28))
+// console.log(vesna (232))
 
 // function number (num){
 //     if (num === 50){
@@ -55,15 +55,54 @@
 
 
 // function minimum(age) {
+//     const numb=Number (age)
 //     if (Number.isInteger(age)) {
 //         if ( age === 20) {
 //             return 'Age is a number'
 //         } else {
 //             return 'Age is not 20'
 //         }
+//     }
+//
+// }
+// console.log(minimum(20))
+
+// function hand (age){
+//     if (typeof age !== 'string'){
+//         return ` ${age} must be string`
+//     }
+//          else {
+//         if (age <= 23) {
+//             return ` ${age} must be alder than 23`
+//         } else {
+//             return 'welcome'
+//         }
+//
+//     }
+//
+// }
+//    console.log (hand (23))
+
+// function handleCheckday (day){
+//     const num = Number (day)
+//     if (day > 7){
+//         return `error`
 //     } else {
-//         return 'Age is not a number'
+//         if (day === 1){
+//             return 'today is monday'
+//         } else if (num === 2) {
+//             return 'today is tuesdey'
+//         } else  if  (num === 3){
+//             return 'today is wendsday'
+//         } else if (num === 4){
+//             return 'today is thursday'
+//         }else if ( num ===5){
+//             return 'today is fridey'
+//         } else {
+//             return 'today is weekend'
+//         }
 //     }
 // }
-// console.log(minimum(434))
+// console.log( handleCheckday(7))
+
 
